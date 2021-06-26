31c1d02725edb6c2b2c1acd188c290a4fa806068
mu-boop <mugheeskhatri6@gmail.com>
2021-06-26 20:41:48 +0500

    work complete

--- src/home.js
+++ src/home.js
@@ -1,16 +1,8 @@
 import Quiz from './quiz';
 import {useSelector} from 'react-redux';
-import {useState , useEffect} from 'react';
+import {useState } from 'react';
 function Home(){
 
-    const [quiz , setQuiz] = useState()
-   fetch('https://jsonplaceholder.typicode.com/posts')
-  .then((response) => response.json())
-  .then((json) => {
-    setQuiz(json)
-  });
-
-  console.log(quiz[0].title)
   let mughees = useSelector(mughees => mughees);  
   console.log(mughees)
   return(
